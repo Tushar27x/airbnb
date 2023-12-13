3c20ba60d80e42292089cdcece98b285f0a377a9
Dev-Tushar27 <sharma.tushar.271003@example.com>
2023-12-13 20:58:11 +0530

    changed next config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,39 @@
 module.exports  = {
     images: {domains: 
         ['res.cloudinary.com'],
+    },
+    async redirects(){
+        return [
+            {
+                source:"/:path",
+                has:[{type:'query', key:'favorites'}],
+                destination: "/favorites",
+                permanent: true
+            },
+            {
+                source:"/:path",
+                has:[{type:'query', key:'trips'}],
+                destination: "/trips",
+                permanent: true
+            },
+            {
+                source:"/:path",
+                has:[{type:'query', key:'listings'}],
+                destination: "/listings",
+                permanent: true
+            },
+            {
+                source:"/:path",
+                has:[{type:'query', key:'reservations'}],
+                destination: "/reservations",
+                permanent: true
+            },
+            {
+                source:"/:path",
+                has:[{type:'query', key:'properties'}],
+                destination: "/properties",
+                permanent: true
+            },
+        ]
     }
 };
